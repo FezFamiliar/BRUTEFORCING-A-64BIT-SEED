931a2a413c0b0e80644d3bc98e0ba017278bda4d
root <root@localhost.localdomain>
2019-12-24 15:02:36 +0000

    final form

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <omp.h>
 #include <limits.h>
+#include <unistd.h>
 unsigned long long state;
 void Setup(unsigned long long seed){
 
@@ -30,10 +31,12 @@ unsigned long long Next(unsigned long long bits){
 		ret <<= 1;
 		ret |= state & 1;
 		for(int k = 0;k < 3;k++){
+			printf("%llu\n",state);
 			state = (state << 1) ^ (state >> 61);
 			state &= ULONG_MAX;
 			state ^= ULONG_MAX;
-
+			printf("%llu\n",state);
+			exit(1);
 			for(int j = 0;j < 64;j+=4){
 				cur = (state >> j) & 15;
 				cur = (cur >> 3) | ((cur >> 2) & 2) | ((cur << 3) & 8) | ((cur << 2) & 4);
@@ -43,20 +46,21 @@ unsigned long long Next(unsigned long long bits){
 
 		}
 	}
-
+	printf("%llu",ret);
+	exit(1);
 	return ret;
 
 }
 
-void try_seed(unsigned int i,clock_t t){
+void try_seed(long i,clock_t t){
 	Setup(i);
 	unsigned long long next1 = Next(64);
 
-	if(i % 1000000 == 0) printf("%u\n",i);
+	if(i % 1000000 == 0) printf("%ld\n",i);
 
 
-	if(next1 == 5697797855707103876UL){
-	        printf("Found! Seed: %u\n",i);
+	if(next1 == 10718270564983086108UL){
+	        printf("Found! Seed: %ld\n",i);
 		t = clock() - t;
 		double time_elapsed = ((double)t)/CLOCKS_PER_SEC;
 		printf("Time: %f",time_elapsed);
@@ -66,16 +70,17 @@ void try_seed(unsigned int i,clock_t t){
 
 int main(){
 
-	unsigned int i;
+	long i;
 
-
-	clock_t t;
+	Setup(10000000);
+	Next(64);
+	/*clock_t t;
 	t = clock();
 	#pragma omp parallel
 	{
 	#pragma omp for
 	for(i = UINT_MAX;i >= 0;i--) try_seed(i,t);
 	}
-
+*/
 	return 0;
 }
